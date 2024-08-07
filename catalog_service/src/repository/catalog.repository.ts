9184336a91c19585d0d55c82d71eb7b07dac08c7
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-08-07 14:40:48 +0530

    bootstrap order_service

--- catalog_service/src/repository/catalog.repository.ts
+++ catalog_service/src/repository/catalog.repository.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { ICatalogRepository } from "../interface/catalogRepository.interface";
 import { Product } from "../models/product.model";
 
